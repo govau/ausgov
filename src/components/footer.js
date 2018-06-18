c93b6ff5169f3527e7b31029c3f3a4356036df98
adamzerella <adamzerella@users.noreply.github.com>
2018-06-18 20:59:30 +1000

    Added boilerplate footer

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import AUfooter, { AUfooterNav, AUfooterEnd } from "@gov.au/footer";
+import AUlinkList from "@gov.au/link-list";
+import Link from "gatsby-link";
+
+import "./footer.scss";
+
+const Footer = () => (
+	<AUfooter dark>
+		<AUfooterNav>
+			<AUlinkList
+				items={[
+					{
+						link: "/about",
+						text: "About",
+					},
+				]}
+			/>
+		</AUfooterNav>
+		<AUfooterEnd>
+			© Commonwealth of Australia. With the exception of the Commonwealth
+			Coat of Arms and where otherwise noted, this work is licensed under
+			the{" "}
+			<a href="https://raw.githubusercontent.com/adamzerella/ausgov/master/LICENSE">
+				MIT license
+			</a>
+		</AUfooterEnd>
+	</AUfooter>
+);
+
+export default Footer;
