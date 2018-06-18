84c7b3966f7f3dd3512de8cf9d499c4c7c56a676
Adam Zerella <adamzerella@users.noreply.github.com>
2018-06-18 16:11:50 +1000

    Removed further boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 
 const IndexPage = () => (
-	<div className="au-body au-body--dark">Welcome to AusGov</div>
+	<div className="au-body au-body--dark"></div>
 );
 
 export default IndexPage;
