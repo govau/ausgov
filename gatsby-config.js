1fb63eec137afda2fe9461480c830e5217542a35
adamzerella <adamzerella@users.noreply.github.com>
2018-06-20 22:27:59 +1000

    Added path prefix to gatsby, added cf manifests and configured circle config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  siteMetadata: {
-    title: 'australia.gov.au',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-catch-links',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
-    },
-    'gatsby-transformer-remark',
-  ],
-}
+	pathPrefix: `/ausgov`,
+	siteMetadata: {
+		title: "australia.gov.au",
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sass",
+		"gatsby-plugin-catch-links",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: "pages",
+			},
+		},
+		"gatsby-transformer-remark",
+	],
+};
