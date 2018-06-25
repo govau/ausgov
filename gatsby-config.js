f5c1000f5e22cc381928cd28c4bba03e64fbe4c1
Adam Zerella <adamzerella@users.noreply.github.com>
2018-06-25 14:13:37 +1000

    Reverted branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: 'australia.gov.au',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-catch-links',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
-    },
-    'gatsby-transformer-remark',
-  ],
-}
+	siteMetadata: {
+		title: "australia.gov.au",
+		siteUrl: `https://ausgov.apps.y.cld.gov.au/`,
+	},
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sass",
+		"gatsby-transformer-remark",
+		"gatsby-plugin-catch-links",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: "pages",
+			},
+		},
+		{
+			resolve: "gatsby-plugin-robots-txt",
+			options: {
+				host: "ausgov.apps.y.cld.gov.au",
+				sitemap: "https://ausgov.apps.y.cld.gov.au/sitemap.xml",
+				policy: [{ userAgent: "*", allow: "/" }],
+			},
+		},
+	],
+};
