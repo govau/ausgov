532b4da77cb7185aadec2f239c10add51d17568d
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-07-20 12:33:45 +1000

    Updated 404 page syntax (tabs not spaces)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import AUbutton from '@gov.au/buttons';
-import "./404.scss"
+import React from "react";
+import AUbutton from "@gov.au/buttons";
+import "./404.scss";
 import Link from "gatsby-link";
 
 const error = () => {
-    return (
-        <div class="row">
-            <div className="col-md-12">
-                <h2 className="error-heading">Sorry, we can't find the page you're looking for</h2>
-                <p>
-                    The page might have been deleted, moved or it might not exist.
-                    Please check you’ve typed the right address.
-                </p>
-                <br />
-                <strong>Error code: 404</strong> <br /> <br />
-                <Link to="/" className="visit-home-link"><AUbutton as='secondary'>
-                    Visit our home page
-                </AUbutton></Link>
-            </div>
-
-        </div>
-    );
+	return (
+		<div class="row">
+			<div className="col-md-12">
+				<h2 className="error-heading">
+					Sorry, we can't find the page you're looking for
+				</h2>
+				<p>
+					The page might have been deleted, moved or it might not
+					exist. Please check you’ve typed the right address.
+				</p>
+				<br />
+				<strong>Error code: 404</strong> <br /> <br />
+				<Link to="/" className="visit-home-link">
+					<AUbutton as="secondary">Visit our home page</AUbutton>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default error;
