c48b4be4a492c9da0589d5de7717f051f59e80d9
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-08-03 13:59:02 +1000

    See changes (#88)

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,12 @@ import "./Card.scss";
 
 const Card = props => {
 	return (
-		<Link to={props.link} className="card-link">
-			<div className={"card card--" + props.color}>
-				<div className="card__text">
-					<h3 className="card__heading">
-						<b>{props.title}</b>
-					</h3>
-					<span className="card__subline">{props.bodyText}</span>
-				</div>
+		<Link to={props.link} className={"card card--" + props.color}>
+			<div className="card__text">
+				<h3 className="card__heading">
+					<b>{props.title}</b>
+				</h3>
+				<span className="card__subline">{props.bodyText}</span>
 			</div>
 		</Link>
 	);
