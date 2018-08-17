d2e13612d5047ec265ed69d8ca19edd9f41e2474
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-08-17 14:57:51 +1000

    Added styles for a links and aubody to public-holiday

--- src/pages/holiday.js
+++ src/pages/holiday.js
@@ -6,14 +6,19 @@ class Holiday extends React.Component {
 	render() {
 		return (
 			<div className="public-holiday">
-				<h1>Public holidays</h1>
-				The following list has been extracted from:{" "}
-				<a href="https://data.gov.au" alt="Australian public holidays">
-					data.gov.au
-				</a>
-				<br />
-				<br />
-				<PublicHoliday showAll={true} />
+				<div className="container au-body">
+					<h1>Public holidays</h1>
+					The following list has been extracted from:{" "}
+					<a
+						href="https://data.gov.au"
+						alt="Australian public holidays"
+					>
+						data.gov.au
+					</a>
+					<br />
+					<br />
+					<PublicHoliday showAll={true} />
+				</div>
 			</div>
 		);
 	}
